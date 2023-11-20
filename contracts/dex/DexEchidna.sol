7028124d094d4d495f07ef23b5fd2cb2984bcebc
grc02 <georgi.chonkov27@gmail.com>
2023-11-20 14:11:02 +0200

    Refactored invariants for 'Dex' and 'config' file

--- contracts/dex/DexEchidna.sol
+++ contracts/dex/DexEchidna.sol
@@ -4,6 +4,8 @@ pragma solidity 0.8.21;
 import {Dex, SwappableToken} from "./Dex.sol";
 
 contract DexEchidna {
+    event Log(uint256);
+
     SwappableToken public token1;
     SwappableToken public token2;
     Dex dex;
@@ -21,6 +23,9 @@ contract DexEchidna {
         token2.approve(address(dex), 100);
         dex.addLiquidity(address(token2), 100);
 
+        token1.approve(address(this), address(dex), type(uint256).max);
+        token2.approve(address(this), address(dex), type(uint256).max);
+
         dex.renounceOwnership();
         dex.approve(address(dex), type(uint256).max);
     }
@@ -29,14 +34,31 @@ contract DexEchidna {
         uint256 balance1 = token1.balanceOf(address(this));
         uint256 balance2 = token2.balanceOf(address(this));
 
+        emit Log(balance1);
+        emit Log(balance2);
+
+        uint256 balance1Dex = token1.balanceOf(address(dex));
+        uint256 balance2Dex = token2.balanceOf(address(dex));
+
+        emit Log(balance1Dex);
+        emit Log(balance2Dex);
+
+        uint256 k = token1.balanceOf(address(dex)) * token2.balanceOf(address(dex));
+
+        emit Log(k);
+
         if (direction) {
-            uint256 swapAmount = amount % balance2;
+            uint256 swapAmount = (amount % balance2) % balance2Dex;
+            emit Log(swapAmount);
             dex.swap(address(token2), address(token1), swapAmount);
         } else {
-            uint256 swapAmount = amount % balance1;
+            uint256 swapAmount = (amount % balance1) % balance1Dex;
+            emit Log(swapAmount);
             dex.swap(address(token1), address(token2), swapAmount);
         }
 
-        assert(token1.balanceOf(address(dex)) >= 90 ether || token2.balanceOf(address(dex)) >= 90 ether);
+        uint256 k2 = token1.balanceOf(address(dex)) * token2.balanceOf(address(dex));
+
+        assert(k <= k2);
     }
 }
